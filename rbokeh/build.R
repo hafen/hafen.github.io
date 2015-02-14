4b60fe47c3710e99dcb25d68982bb70f6a6bfd20
hafen <rhafen@gmail.com>
2015-02-14 01:23:16 -0800

    Update toc handling

--- rbokeh/build.R
+++ rbokeh/build.R
@@ -2,5 +2,9 @@ library(rmarkdown)
 library(packagedocs)
 
 render("index.Rmd", output_format = package_docs(lib_dir = "assets"))
+
+# a <- function_ref_template("rbokeh", "~/Documents/Code/rbokeh", exclude = c("pipe", "scales"))
+# cat(a, file = pipe("pbcopy"))
+
 render("rd.Rmd", output_format = package_docs(lib_dir = "assets"))
 
