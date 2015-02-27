18e173809d4fb766e52f15f634a984816bed0316
hafen <rhafen@gmail.com>
2015-02-27 02:11:25 -0500

    Update

--- rbokeh/build.R
+++ rbokeh/build.R
@@ -1,14 +1,15 @@
 library(rbokeh)
 library(rmarkdown)
 library(packagedocs)
-
 code_path <- "~/Documents/Code/rbokeh"
 
 render("index.Rmd", output_format = package_docs(lib_dir = "assets"))
 check_output("index.html")
+system("open index.html")
 
 render_rd("rd_skeleton.Rmd", "rbokeh", code_path, exclude = c("pipe", "scales", "rbokeh-package"))
 check_output("rd.html")
+system("open rd.html")
 
 # a <- function_ref_template("rbokeh", code_path, exclude = c("pipe", "scales", "rbokeh-package"))
 # cat(a, file = pipe("pbcopy"))
