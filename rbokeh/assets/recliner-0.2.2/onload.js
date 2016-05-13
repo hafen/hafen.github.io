efe48f939a990f68cca5fb5bcf17575962c0cdda
hafen <rhafen@gmail.com>
2016-05-13 10:03:30 -0700

    Add ability to use lazyrmd

--- rbokeh/assets/recliner-0.2.2/onload.js
+++ rbokeh/assets/recliner-0.2.2/onload.js
@@ -2,7 +2,7 @@ $(function() {
   $('.widget-lazyload').recliner({
     attrib: 'data-src', // selector for attribute containing the media src
     throttle: 300,      // millisecond interval at which to process events
-    threshold: 200,     // scroll distance from element before its loaded
+    threshold: 1000,    // scroll distance from element before its loaded
     live: true          // auto bind lazy loading to ajax loaded elements
   });
 });
